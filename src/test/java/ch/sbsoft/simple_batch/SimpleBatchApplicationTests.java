4dff853bcbe014f11b61091507f3015bfe41d6db
Stefano Bianda <stefanobianda@ik.me>
2025-04-22 15:52:53 +0200

    init spring boot

---
+++ src/test/java/ch/sbsoft/simple_batch/SimpleBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.sbsoft.simple_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
